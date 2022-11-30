24ad8816dae07ba4adcfb27a5bc0f4790940908f
ingwerludwig <ingwerflash@gmail.com>
2022-11-30 12:00:20 +0700

    feat: add kost migration

---
+++ database/migrations/2022_11_30_045838_create_kost_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('kost', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->string('nama_kost');
+            $table->string('alamat');
+            $table->string('fasilitas_listrik');
+            $table->string('fasilitas_air');
+            $table->string('no_telp');
+            $table->timestamp('created_at')->useCurrent();
+            $table->timestamp('updated_at')->useCurrent()->useCurrentOnUpdate();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('kost');
+    }
+};
