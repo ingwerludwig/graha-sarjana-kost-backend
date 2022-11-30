97725303328cea10d1a4be6484e5fd52d13aab51
ingwerludwig <ingwerflash@gmail.com>
2022-11-30 12:02:38 +0700

    feat: add kamar kost migration

---
+++ database/migrations/2022_11_30_050033_create_kamar_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('kamar', function (Blueprint $table) {
+            $table->integer('id')->primary();
+            $table->string('fasilitas');
+            $table->string('kamar_mandi');
+            $table->string('harga');
+            $table->uuid('kost_id');
+            $table->timestamp('created_at')->useCurrent();
+            $table->timestamp('updated_at')->useCurrent()->useCurrentOnUpdate();
+            $table->foreign('kost_id')->references('id')->on('kost');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('kamar');
+    }
+};
