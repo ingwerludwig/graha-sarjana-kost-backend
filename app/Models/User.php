20e2629934176512e9f5fcf071284eced71714c7
ingwerludwig <ingwerflash@gmail.com>
2022-11-30 11:39:12 +0700

    feat: add roles model and migration

--- app/Models/User.php
+++ app/Models/User.php
@@ -10,35 +10,27 @@ use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
-    use HasApiTokens, HasFactory, Notifiable;
+    use HasFactory;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array<int, string>
-     */
+    public $incrementing = false;
+
     protected $fillable = [
-        'name',
-        'email',
+        'id',
+        'username',
         'password',
+        'created_at',
+        'updated_at',
+        'role_id'
     ];
 
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var array<int, string>
-     */
-    protected $hidden = [
-        'password',
-        'remember_token',
-    ];
+    protected $table = 'user';
 
-    /**
-     * The attributes that should be cast.
-     *
-     * @var array<string, string>
-     */
-    protected $casts = [
-        'email_verified_at' => 'datetime',
-    ];
+    public static function addAdditionalData(Array $req, $umkm): Array
+    {
+        $req['id'] = Uuid::generate()->string;
+        $req['created_at'] = Carbon::now();
+        $req['updated_at'] = Carbon::now();
+
+        return $req;
+    }
 }
